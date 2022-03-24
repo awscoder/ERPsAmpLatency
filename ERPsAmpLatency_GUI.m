b15d2408a2c17e7883f26818d0699f7b8c677d67
awscoder <38738303+awscoder@users.noreply.github.com>
2022-03-24 10:22:46 -0500

    Add files via upload

--- ERPsAmpLatency_GUI.m
+++ ERPsAmpLatency_GUI.m
@@ -81,6 +81,24 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
               
             %% Regions
             Regions.Chann = app.RegionsEditField.Value;
+            ERPs.Regions.Chann = (split(Regions.Chann, ';'));
+            for n = 1:length(ERPs.Regions.Chann)
+                ERPs.Regions.Chann{n} = (split(ERPs.Regions.Chann{n}, ','));
+            end
+            RegionDictionary = containers.Map(...
+                {'FP1', 'FZ', 'F3', 'F7', 'FT9', 'FC5', 'FC1', 'C3', 'T7', 'TP9', 'CP5', 'CP1', 'PZ',...
+                 'P3','P7','O1','OZ','O2','P4','P8','TP10','CP6','CP2','CZ','C4','T8','FT10','FC6','FC2','F4','F8','FP2'},...
+                {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',...
+                 '14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32'});
+            
+            for i = 1:numel(ERPs.Regions.Chann)
+                for n = 1:length(ERPs.Regions.Chann{i})
+                    ERPs.Regions.Nums{i}{n} = RegionDictionary(ERPs.Regions.Chann{i}{n});
+                end
+                ERPs.Regions.Nums{i} = cellfun(@str2num,ERPs.Regions.Nums{i},'un',0);
+            end
+
+
             Regions.Labels = split(app.RegionLabelsEditField.Value, ';');
             
             % 1) a frontal bin, 
@@ -94,12 +112,19 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
 
             for n = 1:length(ERPs.Ranges) %Adds the stiumulus start time to the upper and lower values of the ERP Ranges
                 for i = 1:2
-                ERPs.Ranges{n,i} = str2double(ERPs.Ranges{n,i}) + StimulousStart;
+                    try
+                        ERPs.Ranges{n,i} = str2double(ERPs.Ranges{n,i}) + StimulousStart;
+                    catch
+                        if n == 2
+                            break
+                        end
+                        ERPs.Ranges = {ERPs.Ranges{1},ERPs.Ranges{2}};
+                        ERPs.Ranges{1,2} = str2double(ERPs.Ranges{1,2}) + StimulousStart;
+                            break
+                    end
                 end
             end
-            
-            ERPs.Regions.Chann = (split(Regions.Chann, ';'));
-            ERPs.Regions.Chann = cellfun(@str2num,ERPs.Regions.Chann,'un',0);
+           
             
             ERPs.Tags = split(app.ERPsEditField.Value, ';');
             
@@ -107,8 +132,12 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             Paradigms = split(Paradigms,';'); %Splits each paradigm into a cell
             Num_Paradigms = length(Paradigms); %Creates a variable equal to the number of paradigms
             
-            XLNameAmp = ['Results-BCI-ERPs-Amplitudes-',datestr(now,'mmmm-dd-yyyy_HH_MM_SS'),'.xlsx'];
-            XLNameLat = ['Results-BCI-ERPs-Latency-',datestr(now,'mmmm-dd-yyyy_HH_MM_SS'),'.xlsx'];
+            ERPsName = '';
+            for i = 1:numel(ERPs.Tags)
+                ERPsName = [ERPsName , char(ERPs.Tags{i})];
+            end
+            XLNameAmp = [ERPsName,'-Amplitudes-',datestr(now,'mmmm-dd-yyyy_HH_MM_SS'),'.xlsx'];
+            XLNameLat = [ERPsName,'-Latency-',datestr(now,'mmmm-dd-yyyy_HH_MM_SS'),'.xlsx'];
             
             
             %% Reading Samples (Edit which participants to skip here(if applicable))
@@ -154,18 +183,20 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
                 xlswrite(XLNameAmp,[[1:PtSize]' , FeatureMat_DiffTvsNT.Peaks ],ERPs.Tags{f},['A' num2str(XLLine)]);
                 xlswrite(XLNameLat,[[1:PtSize]' , FeatureMat_DiffTvsNT.Latency ],ERPs.Tags{f},['A' num2str(XLLine)]);
                 
-                for r = 1:length(ERPs.Regions.Chann) %Makes Regional Sheets
-                    XLLine = 1;
-                    
-                    HeaderInd = (ERPs.Regions.Chann{r} .* ones(Num_Paradigms,1)*Num_Paradigms') - [Num_Paradigms-1:-1:0]';
-                    
-                    xlswrite(XLNameAmp,tmpHeader(:,[1,HeaderInd(:)'+1]),[ERPs.Tags{f} '-' Regions.Labels{r}],['A' num2str(XLLine)]);
-                    xlswrite(XLNameLat,tmpHeader(:,[1,HeaderInd(:)'+1]),[ERPs.Tags{f} '-' Regions.Labels{r}],['A' num2str(XLLine)]);
-            
-                    XLLine = XLLine + size(tmpHeader,1);
-                    
-                    tmpAverages_Peaks = [mean(FeatureMat_DiffTvsNT.Peaks(:,HeaderInd(1,:)),2)];
-                    tmpAverages_Latency = [mean(FeatureMat_DiffTvsNT.Latency(:,HeaderInd(1,:)),2)];
+                for r = 1:length(ERPs.Regions.Nums) %Makes Regional Sheets
+                        XLLine = 1;
+                        HeaderInd = [];
+                    for i = 1:numel(ERPs.Regions.Nums{r})
+                        HeaderInd{i} = (ERPs.Regions.Nums{r}{i} .* ones(Num_Paradigms,1)*Num_Paradigms') - [Num_Paradigms-1:-1:0]';
+                    end
+                        HeaderInd = cat(2,HeaderInd{:});
+                        xlswrite(XLNameAmp,tmpHeader(:,[1,HeaderInd(:)'+1]),[ERPs.Tags{f} '-' Regions.Labels{r}],['A' num2str(XLLine)]);
+                        xlswrite(XLNameLat,tmpHeader(:,[1,HeaderInd(:)'+1]),[ERPs.Tags{f} '-' Regions.Labels{r}],['A' num2str(XLLine)]);
+
+                        XLLine = XLLine + size(tmpHeader,1);
+
+                        tmpAverages_Peaks = [mean(FeatureMat_DiffTvsNT.Peaks(:,HeaderInd(1,:)),2)];
+                        tmpAverages_Latency = [mean(FeatureMat_DiffTvsNT.Latency(:,HeaderInd(1,:)),2)];
                     
                     for o = 2:Num_Paradigms
                         tmpAverages_Peaks = horzcat(tmpAverages_Peaks,[mean(FeatureMat_DiffTvsNT.Peaks(:,HeaderInd(o,:)),2)]); %generates a column vector containing the mean of each row
@@ -182,9 +213,14 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
                 FeatureMat_DiffTvsNT_Regions = [];
                 Paradigm_Num_Array = [1:Num_Paradigms];
                 
-                for r = 1:length(ERPs.Regions.Chann)
-                    ElecPara_Indx = Num_Paradigms*(ERPs.Regions.Chann{r}-1)+Paradigm_Num_Array';
-            
+                for r = 1:length(ERPs.Regions.Nums)
+                    ElecPara_Indx = [];
+                    for i = 1:numel(ERPs.Regions.Nums{r})
+                        ElecPara_Indx{i} = (ERPs.Regions.Nums{r}{i} .* ones(Num_Paradigms,1)*Num_Paradigms') - [Num_Paradigms-1:-1:0]';
+                    end
+                    ElecPara_Indx = cat(2,ElecPara_Indx{:});
+%                     ElecPara_Indx = Num_Paradigms*(ERPs.Regions.Nums{r}{i}-1)+Paradigm_Num_Array';
+                    
                     for o = 1:Num_Paradigms
             
                         FeatureMat_DiffTvsNT_Regions.Peaks(:,Num_Paradigms*(r-1)+o) = mean(FeatureMat_DiffTvsNT.Peaks(:,ElecPara_Indx(o,:)),2);
@@ -196,12 +232,12 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
                 end
                 
                 XLLine = 1;
-                xlswrite(XLNameAmp,tmpHeader,[ERPs.Tags{f} '- Regions'],['A' num2str(XLLine)]);
-                xlswrite(XLNameLat,tmpHeader,[ERPs.Tags{f} '- Regions'],['A' num2str(XLLine)]);
+                xlswrite(XLNameAmp,tmpHeader,[ERPs.Tags{f} '-Regions'],['A' num2str(XLLine)]);
+                xlswrite(XLNameLat,tmpHeader,[ERPs.Tags{f} '-Regions'],['A' num2str(XLLine)]);
                 
                 XLLine = XLLine + size(tmpHeader,1);
-                xlswrite(XLNameAmp,[[1:PtSize]' , FeatureMat_DiffTvsNT_Regions.Peaks ],[ERPs.Tags{f} '- Regions'],['A' num2str(XLLine)]);
-                xlswrite(XLNameLat,[[1:PtSize]' , FeatureMat_DiffTvsNT_Regions.Latency ],[ERPs.Tags{f} '- Regions'],['A' num2str(XLLine)]);
+                xlswrite(XLNameAmp,[[1:PtSize]' , FeatureMat_DiffTvsNT_Regions.Peaks ],[ERPs.Tags{f} '-Regions'],['A' num2str(XLLine)]);
+                xlswrite(XLNameLat,[[1:PtSize]' , FeatureMat_DiffTvsNT_Regions.Latency ],[ERPs.Tags{f} '-Regions'],['A' num2str(XLLine)]);
                 
                 
                 
@@ -212,7 +248,7 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             disp('---------DONE--------');
             
             %% Functions
-            
+
             %Changing Structure BCI raw structure:
             function [Segments, Labels, Fs, ChannInfo] = ImportingBCIData(Filename)
                 LoadedFile = load(Filename);
@@ -277,7 +313,7 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             % Create DatasetDirectoryEditField
             app.DatasetDirectoryEditField = uieditfield(app.Panel2_4, 'text');
             app.DatasetDirectoryEditField.Position = [42 1 631 22];
-            app.DatasetDirectoryEditField.Value = '\\secd.unl.edu\pittLab\VSD_EEG_motion\Data\Participant_EEG_data\P10 RESNA Analysis\Diff-Waves\';
+            app.DatasetDirectoryEditField.Value = '\\secd.unl.edu\pittLab\VSD_EEG_motion\Data\Participant_EEG_data\P12 DiffWaves\';
 
             % Create DataSelectionLabel
             app.DataSelectionLabel = uilabel(app.LeftPanel);
@@ -297,18 +333,18 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             app.NumberofParticipantsEditField = uieditfield(app.LeftPanel, 'numeric');
             app.NumberofParticipantsEditField.Limits = [0 Inf];
             app.NumberofParticipantsEditField.Position = [243 243 42 22];
-            app.NumberofParticipantsEditField.Value = 10;
+            app.NumberofParticipantsEditField.Value = 12;
 
             % Create StimulusStartTimeseg01Label
             app.StimulusStartTimeseg01Label = uilabel(app.LeftPanel);
             app.StimulusStartTimeseg01Label.HorizontalAlignment = 'right';
             app.StimulusStartTimeseg01Label.Position = [344 243 170 22];
-            app.StimulusStartTimeseg01Label.Text = 'Stimulus Start Time (s) e.g. 0.1';
+            app.StimulusStartTimeseg01Label.Text = 'Baseline Duration (s) e.g. 0.2';
 
             % Create StimulusStartTimeseg01EditField_2
             app.StimulusStartTimeseg01EditField_2 = uieditfield(app.LeftPanel, 'numeric');
             app.StimulusStartTimeseg01EditField_2.Position = [529 243 100 22];
-            app.StimulusStartTimeseg01EditField_2.Value = 0.1;
+            app.StimulusStartTimeseg01EditField_2.Value = 0.2;
 
             % Create RegionsEditFieldLabel
             app.RegionsEditFieldLabel = uilabel(app.LeftPanel);
@@ -319,7 +355,7 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             % Create RegionsEditField
             app.RegionsEditField = uieditfield(app.LeftPanel, 'text');
             app.RegionsEditField.Position = [104 196 576 22];
-            app.RegionsEditField.Value = '[2:3,30];[6:9,11:12,22:26,28:29];[13:20]';
+            app.RegionsEditField.Value = 'CP6,CP2;OZ,O1,O2,P7,P4,P8,P3';
 
             % Create RegionLabelsEditFieldLabel
             app.RegionLabelsEditFieldLabel = uilabel(app.LeftPanel);
@@ -330,7 +366,7 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             % Create RegionLabelsEditField
             app.RegionLabelsEditField = uieditfield(app.LeftPanel, 'text');
             app.RegionLabelsEditField.Position = [136 144 544 22];
-            app.RegionLabelsEditField.Value = 'Frontal;Central;Parietal-Occipital';
+            app.RegionLabelsEditField.Value = 'Central;Parietal-Occipital';
 
             % Create ERPsEditFieldLabel
             app.ERPsEditFieldLabel = uilabel(app.LeftPanel);
@@ -341,7 +377,7 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             % Create ERPsEditField
             app.ERPsEditField = uieditfield(app.LeftPanel, 'text');
             app.ERPsEditField.Position = [91 92 208 22];
-            app.ERPsEditField.Value = 'P300;N170';
+            app.ERPsEditField.Value = 'P100';
 
             % Create ParadigmsFromFilenamesEditFieldLabel
             app.ParadigmsFromFilenamesEditFieldLabel = uilabel(app.LeftPanel);
@@ -374,7 +410,7 @@ classdef ERPsAmpLatency_GUI < matlab.apps.AppBase
             % Create ERPRangesEditField
             app.ERPRangesEditField = uieditfield(app.LeftPanel, 'text');
             app.ERPRangesEditField.Position = [408 92 273 22];
-            app.ERPRangesEditField.Value = '0.200,0.700;0.090,0.230';
+            app.ERPRangesEditField.Value = '0.070,0.205';
 
             % Create RunButton
             app.RunButton = uibutton(app.LeftPanel, 'push');
